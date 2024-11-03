d0dc26bb38bf50bb49db8d166b84e83cfd05fc27
Michael Hurwitz <amh77054@gmail.com>
2024-11-03 18:51:10 +0200

    updated user to includ encription

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,12 @@
 // src/models/User.ts
 import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface IUser extends Document {
   username: string;
   password: string;
   role: string;
+  comparePassword: (candidatePassword: string) => Promise<boolean>;
 }
 
 const userSchema: Schema = new Schema({
@@ -12,5 +14,17 @@ const userSchema: Schema = new Schema({
   password: { type: String, required: true },
   role: { type: String, required: true },
 });
+
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 export default mongoose.model<IUser>("User", userSchema);
